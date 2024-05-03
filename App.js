bbf884cbfe909112b2fb61f59268f4e3b0c44ea8
Stefany Souza <sttefanyss14@gmail.com>
2024-05-03 14:57:48 -0300

    Ap filme em react native

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState} from 'react';
+import { View, Text, StyleSheet, FlatList} from 'react-native';
+
+import api from './src/services/api';
+import Filmes from './src/Filmes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(()=> {
+
+    async function loadFilmes(){
+      const response = await api.get('r-api/?api=filmes');
+      // console.log(response.data);
+      setFilmes(response.data);
+    }
+
+    loadFilmes();
+
+  }, []);
+  
+
+ return (
+   <View style={styles.container}>
+     
+     <FlatList
+     data={filmes}
+     keyExtractor={ item => String(item.id) }
+     renderItem={ ({ item }) => <Filmes data={item} /> }
+     />
+
+   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container:{
+    flex:1,
+  }
 });
